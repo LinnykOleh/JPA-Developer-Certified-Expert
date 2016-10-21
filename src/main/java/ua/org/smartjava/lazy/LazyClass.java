96ce67bea6bf6a23ad97be39c13334f42203b53e
tsypuk <tsypuk.rb@gmail.com>
2016-10-21 11:22:37 +0300

    Lazy loading; added owasp10 check

---
+++ src/main/java/ua/org/smartjava/lazy/LazyClass.java
@@ -0,0 +1,26 @@
+package ua.org.smartjava.lazy;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Lazy")
+public class LazyClass {
+    @Id
+    private long id;
+
+    @Getter @Setter
+    private String name;
+
+//  This does not work with hibernate implementation.
+//    TODO check why ?
+    @Basic(fetch = FetchType.LAZY)
+    @Getter @Setter
+    private String position;
+}
