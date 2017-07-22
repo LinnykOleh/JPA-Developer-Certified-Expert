bf892c16bd61a4ebd5e62a2bfcbfdcf976534041
tsypuk <tsypuk.rb@gmail.com>
2017-07-22 18:05:08 +0300

    added dynamic criteria

--- criteria-api/src/test/java/ua/in/smartjava/domain/CriteriaTest.java
+++ criteria-api/src/test/java/ua/in/smartjava/domain/CriteriaTest.java
@@ -15,6 +15,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.ParameterExpression;
 import javax.persistence.criteria.Root;
 
 @RunWith(SpringRunner.class)
@@ -29,27 +30,16 @@ public class CriteriaTest {
     @Before
     public void init() {
         this.entityManager = this.entityManagerFactory.createEntityManager();
-    }
 
-    @Test
-    public void getContextIsWorking() throws Exception {
+        //Prepare data
         entityManager.getTransaction().begin();
-
-        Address bestStreet = Address.builder().street("BestStreet").build();
-        entityManager.persist(bestStreet);
-
+        entityManager.persist(Address.builder().street("New").build());
         entityManager.getTransaction().commit();
-
-        Assert.assertEquals("BestStreet", bestStreet.getStreet());
     }
 
     @Test
     public void testCriteria() {
         // Given
-        entityManager.getTransaction().begin();
-        entityManager.persist(Address.builder().street("New").build());
-        entityManager.getTransaction().commit();
-
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Address> criteriaQuery = criteriaBuilder.createQuery(Address.class);
         Root<Address> root = criteriaQuery.from(Address.class);
@@ -63,6 +53,27 @@ public class CriteriaTest {
         List<Address> resultList = query.getResultList();
         Assert.assertEquals(1, resultList.size());
         Assert.assertEquals("New", resultList.get(0).getStreet());
+    }
+
+    @Test
+    public void testDynamicCriteria() {
+        // Given
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Address> criteriaQuery = criteriaBuilder.createQuery(Address.class);
+        Root<Address> root = criteriaQuery.from(Address.class);
+        criteriaQuery.select(root)
+                .distinct(true);
+
+        ParameterExpression<String> streetName = criteriaBuilder.parameter(String.class, "name");
+        criteriaQuery.where(criteriaBuilder.equal(root.get("street"), streetName));
+
+        // When
+        TypedQuery<Address> typedQuery = entityManager.createQuery(criteriaQuery);
+        typedQuery.setParameter("name", "New");
+        List<Address> resultList = typedQuery.getResultList();
+
+        // Then
+        Assert.assertEquals(1, resultList.size());
     }
 
 }
