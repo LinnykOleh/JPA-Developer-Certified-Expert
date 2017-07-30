4fba71db3a01cba3fe69e3361a09bd5ba4c550a2
tsypuk <tsypuk.rb@gmail.com>
2017-07-30 12:30:34 +0300

    added module for collection mappings

---
+++ collection/src/test/java/ua/in/smartjava/domain/EmployeeTest.java
@@ -0,0 +1,38 @@
+package ua.in.smartjava.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmployeeTest {
+
+    @Autowired
+    private EntityManagerFactory entityManagerFactory;
+
+    private EntityManager entityManager;
+
+    @Before
+    public void init() {
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
+    /**
+     * Hibernate: insert into employee (id, name, bonded, top_skilled) values (null, ?, ?, ?)
+     * Hibernate: insert into employee_secured (employee_id, secured) values (?, ?)
+     * Hibernate: insert into employee_secured (employee_id, secured) values (?, ?)
+     */
+    @Test
+    public void test() {
+    }
+
+}
