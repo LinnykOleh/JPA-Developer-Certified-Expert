27c6348b094425061271580f2e0d57b2280764b4
tsypuk <tsypuk.rb@gmail.com>
2017-08-01 20:14:46 +0300

    cleanup

--- validation/src/main/java/ua/in/smartjava/domain/Employee.java
+++ validation/src/main/java/ua/in/smartjava/domain/Employee.java
@@ -1,32 +1,19 @@
 package ua.in.smartjava.domain;
 
-import org.springframework.context.event.EventListener;
-
 import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.PostLoad;
-import javax.persistence.PostPersist;
-import javax.persistence.PostRemove;
-import javax.persistence.PostUpdate;
-import javax.persistence.PrePersist;
-import javax.persistence.PreRemove;
-import javax.persistence.PreUpdate;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import javax.validation.groups.Default;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import ua.in.smartjava.constraint.Street;
 import ua.in.smartjava.validationGroups.Personal;
 import ua.in.smartjava.validationGroups.StreetGroup;
