f4268262895018aa66c1a77458e93eba5442b1c8
tsypuk <tsypuk.rb@gmail.com>
2017-08-06 14:58:52 +0300

    refactored metamodel

--- criteria-api/src/test/java/ua/in/smartjava/domain/MetamodelTest.java
+++ criteria-api/src/test/java/ua/in/smartjava/domain/MetamodelTest.java
@@ -1,27 +1,31 @@
 package ua.in.smartjava.domain;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Set;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+import javax.persistence.metamodel.Attribute;
 import javax.persistence.metamodel.EntityType;
 import javax.persistence.metamodel.Metamodel;
 import javax.persistence.metamodel.Type;
 
 import lombok.extern.slf4j.Slf4j;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Slf4j
 public class MetamodelTest {
 
-    @Autowired
+    @PersistenceUnit
     private EntityManagerFactory entityManagerFactory;
 
     private EntityManager entityManager;
@@ -35,13 +39,19 @@ public class MetamodelTest {
     public void test() {
         // Given
         Metamodel metamodel = entityManager.getMetamodel();
+        metamodel.getManagedTypes().forEach(type -> System.err.println(String.valueOf(type)));
 
         // When
         EntityType<Address> entityType = metamodel.entity(Address.class);
 
         // Then
-        Assert.assertEquals(Type.PersistenceType.ENTITY, entityType.getPersistenceType());
-        log.error(String.valueOf(entityType.getAttributes()));
-        Assert.assertEquals(Address.class, entityType.getJavaType());
+        assertEquals(Type.PersistenceType.ENTITY, entityType.getPersistenceType());
+        assertEquals(Address.class, entityType.getJavaType());
+
+        Set<Attribute<? super Address, ?>> attributes = entityType.getAttributes();
+        assertEquals(3, attributes.size());
+        for (Attribute attribute : attributes) {
+            log.error(String.valueOf(attribute.getJavaType()));
+        }
     }
 }
