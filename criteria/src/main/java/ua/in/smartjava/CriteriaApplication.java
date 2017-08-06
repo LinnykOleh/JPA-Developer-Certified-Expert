5fcc4eac4a2ca04821d423772e815053b95997b8
tsypuk <tsypuk.rb@gmail.com>
2017-08-06 14:05:02 +0300

    added criteria module

---
+++ criteria/src/main/java/ua/in/smartjava/CriteriaApplication.java
@@ -0,0 +1,12 @@
+package ua.in.smartjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriteriaApplication.class, args);
+	}
+}
